d1b29dc32d797c3e627745b5f9f86a53f047e15b
Neeraj Kumar <neerajchaudhary0699@gmail.com>
2021-03-13 01:19:45 +0530

    Write First TestCase

--- src/string_calculator/Tester.java
+++ src/string_calculator/Tester.java
@@ -1,5 +1,16 @@
 package string_calculator;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 public class Tester {
+
+	public static void main(String[] args) {
+		org.junit.runner.JUnitCore.main("string_calculator.Test");
+    }
+	@Test
+	public void testNumberWithValueMoreThanThausand() {
+		assertEquals(2,StringCalculator.Add("2,1001"));
+	}
+	
+	
 
 }
