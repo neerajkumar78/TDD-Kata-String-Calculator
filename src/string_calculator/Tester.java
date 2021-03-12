d0f318892bcf6a5ef97d6ac61dd932a794e92e4e
Neeraj Kumar <neerajchaudhary0699@gmail.com>
2021-03-13 01:32:11 +0530

    write 3rd testcase

--- src/string_calculator/Tester.java
+++ src/string_calculator/Tester.java
@@ -6,10 +6,12 @@ public class Tester {
 	public static void main(String[] args) {
 		org.junit.runner.JUnitCore.main("string_calculator.Test");
     }
+	
 	@Test
 	public void testNumberWithValueMoreThanThausand() {
 		assertEquals(2,StringCalculator.Add("2,1001"));
 	}
+	
 	@Test
 	public void testNumberWithNegativeValue() {
 		try {
@@ -18,6 +20,11 @@ public class Tester {
 		catch(IllegalArgumentException e) {
 		assertEquals(e.getMessage(),"negative not allowed: -1, -2, -3");
 	}
+	}
+	
+	@Test
+	public void testDefaultDelimiter() {
+		assertEquals(6,StringCalculator.Add("1\n2,3"));
 	}
 	
 
