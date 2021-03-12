b3528350a0876f576ce0b7b8f7e6e38698f1a908
Neeraj Kumar <neerajchaudhary0699@gmail.com>
2021-03-13 01:28:16 +0530

    write tetcase for number with negative value

--- src/string_calculator/Tester.java
+++ src/string_calculator/Tester.java
@@ -10,7 +10,15 @@ public class Tester {
 	public void testNumberWithValueMoreThanThausand() {
 		assertEquals(2,StringCalculator.Add("2,1001"));
 	}
-	
+	@Test
+	public void testNumberWithNegativeValue() {
+		try {
+			StringCalculator.Add("2,-1,-2,-3");
+		}
+		catch(IllegalArgumentException e) {
+		assertEquals(e.getMessage(),"negative not allowed: -1, -2, -3");
+	}
+	}
 	
 
 }
